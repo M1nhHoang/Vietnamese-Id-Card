4e1b4b8917c9f551c36b89473cb4e70a3220b9b4
M1nhHoang <boydammedamsamcotdien@gmail.com>
2023-05-13 18:54:22 +0700

    first commit

---
+++ Transformer_OCR/vietOCR.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import cv2
+from vietocr.tool.predictor import Predictor
+from vietocr.tool.config import Cfg
+
+config = Cfg.load_config_from_name('vgg_transformer')
+
+config['weights'] = './transformerocr.pth'
+config['weights'] = 'https://drive.google.com/uc?id=13327Y1tz1ohsm5YZMyXVMPIOjoOA0OaA'
+config['cnn']['pretrained']=False
+config['device'] = 'cpu'
+config['predictor']['beamsearch']=False
+
+detector = Predictor(config)
+
+def convert_img_to_text(img):
+	# predict
+	result = detector.predict(img)
+	return result
